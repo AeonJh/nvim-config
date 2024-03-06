4bb133b31e0569a78c6e4f91694ddcb2750dc11c
AeonJh Fu <aeonjh@nanoleaf.me>
2024-03-06 11:51:55 +0800

    chg: chg theme and install auto-save.

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,15 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  "Pocco81/auto-save.nvim",
+  {
+    name = "auto-save",
+    event = "InsertEnter",
+	  config = function()
+		   require("auto-save").setup {
+			  -- your config goes here
+			  -- or just leave it empty :)
+		   }
+	  end,
+  },
 }
