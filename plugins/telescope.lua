19bc1d1088cf9a3d9d558ab8e3346cebcaf91007
AeonJh <2423812668@qq.com>
2024-03-08 14:36:42 +0800

    new: add live_grep_args.

---
+++ plugins/telescope.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+      },
+    },
+    keys = {
+      {
+        "<leader>fwa",
+        "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+        desc = "live_grep_args (root dir)",
+      },
+    },
+    config = function(_, opts)
+      local telescope = require("telescope")
+      telescope.setup(opts)
+      telescope.load_extension("live_grep_args")
+    end,
+  },
+}
